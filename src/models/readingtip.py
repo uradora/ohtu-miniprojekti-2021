bb465eee293bac0dc57c48a451bde4a7c7178e3d
Roope Salmi <rpsalmi@gmail.com>
2021-04-13 15:47:50 +0300

    Add http:// prefix in model, tests

--- src/models/readingtip.py
+++ src/models/readingtip.py
@@ -1,4 +1,6 @@
 class ReadingTip:
     def __init__(self, title, link):
         self.title = title
+        if not link.startswith("http://") and not link.startswith("https://"):
+            link = "http://" + link
         self.link = link
--- src/tests/models/readingtip_test.py
+++ src/tests/models/readingtip_test.py
@@ -6,3 +6,11 @@ class TestReadingTip(unittest.TestCase):
         readingtip = ReadingTip("Hyvä kirja", "https://kirjakauppa.fi/123")
         self.assertEqual(readingtip.title, "Hyvä kirja")
         self.assertEqual(readingtip.link, "https://kirjakauppa.fi/123")
+
+    def test_constructor_adds_http(self):
+        readingtip = ReadingTip("Keskinkertainen kirja", "kirjakauppa.fi/124")
+        self.assertEqual(readingtip.link, "http://kirjakauppa.fi/124")
+
+    def test_constructor_preserves_http(self):
+        readingtip = ReadingTip("Huono kirja", "http://kirjakauppa.fi/125")
+        self.assertEqual(readingtip.link, "http://kirjakauppa.fi/125")
