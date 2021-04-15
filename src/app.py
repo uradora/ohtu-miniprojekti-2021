f7e485eaf725546c9491f5b3f4e5e54d15a55364
SP <sapak@protonmail.com>
2021-04-15 12:59:43 +0300

    Add Flask-SQLAlchemy, psycopg2 & configuration

--- src/app.py
+++ src/app.py
@@ -1,9 +1,24 @@
 from flask import Flask
-
-
-
 app = Flask(__name__)
 
 app.secret_key = "SECRET_KEY"
 
+from flask_sqlalchemy import SQLAlchemy
+import os
+
+if os.environ.get("HEROKU"):
+    app.config["SQLALCHEMY_DATABASE_URI"] = os.environ.get("DATABASE_URL")
+else:
+    app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///tips.db"
+    #prints SQL-queries
+    app.config["SQLALCHEMY_ECHO"] = True
+
+
+db = SQLAlchemy(app)
+
 import routes
+
+try:
+    db.create_all()
+except:
+    pass
