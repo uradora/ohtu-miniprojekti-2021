fbe8722a9f0ef1415456bb9a32593917f451e9f9
uradora <meri.rajamaki@helsinki.fi>
2021-04-17 06:23:36 +0300

    added routes and templates for: login, logout, register

--- src/routes.py
+++ src/routes.py
@@ -6,7 +6,7 @@ from flask import redirect, render_template, request, flash
 def newtip():
     title = request.form["title"]
     if readingtip_service.contains_title(title):
-        flash(f"Tips already contains tip with title {title}")
+        flash(f"Lukuvinkit sisältävät jo vinkin otsikolla {title}")
         return redirect("/form")
     readingtip_service.create_tip(title, request.form["link"])
     return redirect("/")
@@ -19,3 +19,36 @@ def form():
 def index():
     tips=readingtip_service.get_tips()
     return render_template("index.html", tips=tips)
+
+@app.route("/login", methods=["GET","POST"])
+def login():
+    if request.method == "GET":
+        return render_template("login.html")
+    if request.method == "POST":
+        username = request.form["username"]
+        password = request.form["password"]
+        if user_service.login(username,password):
+            return redirect("/")
+        else:
+            flash(f"Kirjautuminen epäonnistui")
+            return redirect("/")
+
+@app.route("/logout")
+def logout():
+    user_service.logout()
+    return redirect("/")
+
+@app.route("/register", methods=["GET","POST"])
+def register():
+    if request.method == "GET":
+        return render_template("register.html")
+    if request.method == "POST":
+        username = request.form["username"]
+        password = request.form["password"]
+        if user_service.register(username,password):
+            return redirect("/")
+        else:
+            flash(f"Rekisteröityminen epäonnistui")
+            return redirect("/")
+
+
