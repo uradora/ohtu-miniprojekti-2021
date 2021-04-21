b9efa533b2ec5cbc432c605f8948ee09eab1462f
Roope Salmi <rpsalmi@gmail.com>
2021-04-21 23:49:09 +0300

    Add tests

--- src/tests/services/readingtip_service_test.py
+++ src/tests/services/readingtip_service_test.py
@@ -7,14 +7,25 @@ class ReadingTipRepositoryStub:
     def __init__(self):
         tips = []
         self._tips = tips
+        self._id_counter = 1
 
     def get_tips(self, user):
         return [t for t in self._tips if t.user == user]
 
-    def create_tip(self, tip):
-        self._tips.append(tip)
+    def get_tip(self, tip_id):
+        for tip in self._tips:
+            if tip.id == tip_id:
+                return tip
+        return None
 
+    def create_tip(self, tip):
+        tip.id = self._id_counter
+        self._id_counter += 1
+        self._tips.append(tip)
         return tip
+
+    def delete_tip(self, tip):
+        self._tips = [t for t in self._tips if t != tip]
 
     def contains_title(self, user, title):
         for readingtip in self.get_tips(user):
@@ -23,12 +34,13 @@ class ReadingTipRepositoryStub:
         return False
 
 
-class TestReadingTip(unittest.TestCase):
+class TestReadingTipService(unittest.TestCase):
     def setUp(self):
         self.repository = ReadingTipRepositoryStub()
         self.login = LoginServiceStub()
         self.service = ReadingTipService(self.repository, self.login)
         self.user = User("maija", "yah2Oozo")
+        self.user.id = 1
         self.login.login_user(self.user)
 
     def test_create_adds_to_collection(self):
@@ -42,4 +54,19 @@ class TestReadingTip(unittest.TestCase):
 
     def test_contains_title_if_present(self):
         self.service.create_tip("Hyvä kirja", "kirjakauppa.fi/123")
+        assert self.service.contains_title("Hyvä kirja")
+
+    def test_can_delete_own_tip(self):
+        tip = self.service.create_tip("Hyvä kirja", "kirjakauppa.fi/123")
+        assert self.service.delete_tip(tip.id)
+        assert not self.service.contains_title("Hyvä kirja")
+
+    def test_cannot_delete_others_tip(self):
+        other_user = User("mikko", "yah2Oozo")
+        other_user.id = 2
+        self.login.login_user(other_user)
+        tip = self.service.create_tip("Hyvä kirja", "kirjakauppa.fi/123")
+        self.login.login_user(self.user)
+        assert not self.service.delete_tip(tip.id)
+        self.login.login_user(other_user)
         assert self.service.contains_title("Hyvä kirja")
