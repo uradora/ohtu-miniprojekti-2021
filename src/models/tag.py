7bd68e3ccf0ed235c9473a74b7cbc4927a126dfa
Roope Salmi <rpsalmi@gmail.com>
2021-04-27 11:16:00 +0300

    Validate tag name

--- src/models/tag.py
+++ src/models/tag.py
@@ -8,3 +8,9 @@ class Tag(db.Model):
     def __init__(self, name, **kwargs):
         super().__init__(**kwargs)
         self.name = name
+
+    @validates("name")
+    def validate_name(self, _key, name):
+        assert len(name) > 0, "Tag name must not be empty"
+        assert len(name) <= 80, "Tag name is too long"
+        return name
